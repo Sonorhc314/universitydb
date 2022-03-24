6e0eccd610467d4a1bdf2e56df8cc6b5f60241a5
Viktoriia Rybalka <neyviktorii@gmail.com>
2022-03-24 15:23:44 +0100

    third commit

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import datas_init as data
-import menu_user as user
-
-def menu():
-    print("""Choose an action\n 
-            1)Create a new account\n
-            2)See all Faculties\n
-            3)See all students\n
-            4)Log in\n
-            ENTER "q" TO QUIT
-            """)
 
 
 def fetcher():
@@ -17,12 +7,46 @@ def fetcher():
         print(x)
 
 
-choice = 0
+def tuition(gpa_t):
+    if gpa_t >= 80:
+        print("You have tuition")
+    else:
+        print(f"No tuition, your GPA({gpa_t}) has to be {80-gpa_t} points higher")
 
 
-while(choice != 'q'):
+choice, flag = 0, 0
+facult_log, gpa_log, id_log = 1, 60, 0
+
+
+def menu():
+    print("""
+            Choose an action\n 
+            1)Create a new account\n
+            2)See all Faculties\n
+            3)See all students\n
+            4)Find a student""")
+    if flag == 1:
+        print("""
+            5)See your faculty's rating list\n
+            6)Check if you have a tuition\n
+            7)Update password\n
+            8)Log out
+            """)
+    else:
+        print("""
+            5)Log in\n
+                """)
+    print("""
+            To receive tuition student's gpa has to be >= 80\n
+            ENTER 'q' TO QUIT""")
+
+
+while choice != 'q':
     menu()
-    choice = input('1-4 : ')
+    if flag == 0:
+        choice = input('1-5 : ')
+    else:
+        choice = input('1-8 : ')
     if choice == '1':
         name = input("Enter Name: ")
         sur = input("Enter Surname: ")
@@ -39,10 +63,10 @@ while(choice != 'q'):
         fetcher()
         data.cursorObject.execute("SELECT COUNT(*) FROM Student WHERE FacultyID=1")
         records = data.cursorObject.fetchall()
-        print('1)', records[0], 'Students')
+        print('1)', records[0][0], 'Students')
         data.cursorObject.execute("SELECT COUNT(*) FROM Student WHERE FacultyID=2")
         records = data.cursorObject.fetchall()
-        print("2)", records[0], 'Students')
+        print("2)", records[0][0], 'Students')
     elif choice == '3':
         data.cursorObject.execute("SELECT ID, Name, Surname, FacultyID, University.Faculty, "
                                   "Student.SpecID, "
@@ -56,28 +80,54 @@ while(choice != 'q'):
                                   "ORDER BY 4,6,3")
         fetcher()
     elif choice == '4':
-        id = int(input("Your ID(check in 'see all students'): "))
+        name_find = input("Name: ")
+        sur_find = input("Surname: ")
+        data.cursorObject.execute(f"SELECT ID, Name, Surname, FacultyID, SpecID FROM Student "
+                                  f"WHERE Name = '{name_find}' AND Surname = '{sur_find}'")
+        fetcher()
+    elif choice == '5' and flag == 0:
+        id_log = int(input("Your ID(check in 'see all students'): "))
         passw1 = input("Password: ")
         data.cursorObject.execute("SELECT COUNT(*) FROM Student WHERE FacultyID=1")
         records_count = data.cursorObject.fetchall()
-        id_1 = int(records_count[0][0])
+        id_1 = records_count[0][0]
         data.cursorObject.execute("SELECT COUNT(*) FROM Student WHERE FacultyID=2")
         records_count = data.cursorObject.fetchall()
-        id_2 = int(records_count[0][0])
+        id_2 = records_count[0][0]
         id_all = id_1+id_2
-        if(id_all >= id):
-            data.cursorObject.execute(f"SELECT Password FROM Student WHERE ID = {id}")
+        if id_all >= id_log:
+            data.cursorObject.execute(f"SELECT Password FROM Student WHERE ID = {id_log}")
             pass_sql = data.cursorObject.fetchone()[0]
-            if(pass_sql == passw1):
-                data.cursorObject.execute(f"SELECT Name, Surname FROM Student WHERE ID = {id}")
+            if pass_sql == passw1:
+                data.cursorObject.execute(f"SELECT Name, Surname, FacultyID, GPA FROM Student WHERE ID = {id_log}")
                 records_log = data.cursorObject.fetchall()
                 name_log = records_log[0][0]
                 sur_log = records_log[0][1]
+                facult_log = records_log[0][2]
+                gpa_log = records_log[0][3]
                 print("You are logged as", name_log, sur_log)
+                flag = 1
             else:
                 print("Either your ID or your password is invalid!")
         else:
             print("Either your ID or your password is invalid!")
+    elif choice == '5' and flag == 1:
+        data.cursorObject.execute("SELECT ID, Name, Surname, GPA, FacultyID, University.Faculty, "
+                                  "Student.SpecID, "
+                                  f"faculty{facult_log}.Spec FROM STUDENT "
+                                  f"INNER JOIN faculty{facult_log} USING(FacultyID, SpecID)"
+                                  "INNER JOIN University USING(FacultyID)"
+                                  "ORDER BY GPA")
+        fetcher()
+    elif choice == '6' and flag == 1:
+        print("Your tuition status: ", tuition(gpa_log))
+    elif choice == '7' and flag == 1:
+        new_pass = input("Your new password:")
+        data.cursorObject.execute(f"UPDATE Student SET Password = {new_pass} WHERE ID = {id_log}")
+        print("Your password was updated")
+    elif choice == '8' and flag == 1:
+        flag = 0
+        print("Logged out successfully")
     else:
         continue
 
--- menu_user.py
+++
@@ -1,4 +0,0 @@
-def menu():
-    update_pass = print("update")
-    see_students = print("see")
-    return 0
